ffd43392e2f58871578b336331c55c1281adee84
MIYaN1012 <yy690730@163.com>
2024-07-06 23:15:52 +0800

    chore: edit misc files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,23 @@
 import { defineConfig } from 'astro/config';
+import { rehypeShiki } from '@astrojs/markdown-remark'
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math'; 
+import rehypeKatex from 'rehype-katex';
 import { resolve } from 'path';
 import {
   transformerNotationDiff,
   transformerNotationHighlight,
 } from '@shikijs/transformers'
-import { remarkReadingTime, remarkModifiedTime } from './scripts/remark-plugins.mjs';
+import {
+  remarkReadingTime,
+  remarkModifiedTime,
+} from './scripts/remark-plugins.mjs';
+
 
 const DIR = import.meta.dirname;
+console.info(resolve(DIR, './src/styles/mermaid.css'))
 
 // https://astro.build/config
 export default defineConfig({
@@ -32,14 +38,22 @@ export default defineConfig({
   markdown: {
     shikiConfig: {
       theme: 'github-light',
+      
       transformers: [
         transformerNotationDiff(),
         transformerNotationHighlight(),
       ]
     },
-    remarkPlugins: [remarkMath, remarkModifiedTime, remarkReadingTime],
-    rehypePlugins: [rehypeKatex],
-    syntaxHighlight: 'shiki',
+    remarkPlugins: [
+      remarkMath,
+      remarkModifiedTime,
+      remarkReadingTime,
+    ],
+    rehypePlugins: [
+      rehypeKatex,
+      rehypeShiki,
+    ],
+    syntaxHighlight: false,
     // gfm: true,
   },
   integrations: [
