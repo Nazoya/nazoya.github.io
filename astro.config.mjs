1fd4aae188da2159de54d1adac5d4c2508895986
MIYaN1012 <yy690730@163.com>
2024-07-10 23:18:09 +0800

    feature: add sandpack and react support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,67 +1,46 @@
 import { defineConfig } from 'astro/config';
-import { rehypeShiki } from '@astrojs/markdown-remark'
+import { rehypeShiki } from '@astrojs/markdown-remark';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import remarkMath from 'remark-math'; 
+import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import { resolve } from 'path';
-import {
-  transformerNotationDiff,
-  transformerNotationHighlight,
-} from '@shikijs/transformers'
-import {
-  remarkReadingTime,
-  remarkModifiedTime,
-} from './scripts/remark-plugins.mjs';
-
-
+import { transformerNotationDiff, transformerNotationHighlight } from '@shikijs/transformers';
+import { remarkReadingTime, remarkModifiedTime } from './scripts/remark-plugins.mjs';
+import react from "@astrojs/react";
 const DIR = import.meta.dirname;
-console.info(resolve(DIR, './src/styles/mermaid.css'))
+console.info(resolve(DIR, './src/styles/mermaid.css'));
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nazoya.github.io/',
   build: {
     rollupOptions: {
-      external: ['/pagefind/pagefind.js'],
-    },
+      external: ['/pagefind/pagefind.js']
+    }
   },
   vite: {
     assetsInclude: '**/pagefind.js',
     resolve: {
       alias: {
-        '@': resolve(DIR, 'src'),
-      },
-    },
+        '@': resolve(DIR, 'src')
+      }
+    }
   },
   markdown: {
     shikiConfig: {
       theme: 'github-light',
-      
-      transformers: [
-        transformerNotationDiff(),
-        transformerNotationHighlight(),
-      ]
+      transformers: [transformerNotationDiff(), transformerNotationHighlight()]
     },
-    remarkPlugins: [
-      remarkMath,
-      remarkModifiedTime,
-      remarkReadingTime,
-    ],
-    rehypePlugins: [
-      rehypeKatex,
-      rehypeShiki,
-    ],
-    syntaxHighlight: false,
+    remarkPlugins: [remarkMath, remarkModifiedTime, remarkReadingTime],
+    rehypePlugins: [rehypeKatex, rehypeShiki],
+    syntaxHighlight: false
     // gfm: true,
   },
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind({
-      applyBaseStyles: false,
-      nesting: true,
-    }),
-  ],
+  integrations: [mdx(), sitemap(), tailwind({
+    applyBaseStyles: false,
+    nesting: true
+  }), react()]
 });
